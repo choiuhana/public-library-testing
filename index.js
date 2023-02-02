407faf7fc3b1e6f544f82f11d0ff319afaf878e6
창원 <cw.choi@soldout.co.kr>
2023-02-02 14:01:56 +0900

    index.tsx -> index.js
    - Android빌드시 index.js를 찾아서 수정

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppRegistry } from "react-native";
 import App from "./src/script/App";
 import { name as appName } from "./src/script/app.json";
 
-const Root = (): JSX.Element => {
+const Root = () => {
   return (
     <>
       <App />
