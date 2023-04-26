03ee07b02897eeb3356bfe1a08953fb436c548fd
최창원 <choiuhana@gmail.com>
2023-04-26 17:09:54 +0900

    테스트 텍스트 제거

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}hello
+        {title}
       </Text>
       <Text
         style={[
